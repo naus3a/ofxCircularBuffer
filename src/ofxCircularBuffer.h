728a382b5f9c396bbe08162d1e5d51b2bbb0a27f
enrico viola <naus3a@gmail.com>
2017-01-31 16:55:56 +0100

    virtual functions

--- src/ofxCircularBuffer.h
+++ src/ofxCircularBuffer.h
@@ -20,47 +20,47 @@ public:
         curPos = 0;
     }
     
-    void setSize(int sz){
+    virtual void setSize(int sz){
         buf.clear();
         buf.resize(sz);
     }
     
-    bool setCurPos(int idx){
+    virtual bool setCurPos(int idx){
         if(idx<0 || idx>=buf.size())return false;
         curPos = idx;
         return true;
     }
     
-    void setValue(T val, int idx){
+    virtual void setValue(T val, int idx){
         buf[idx] = val;
     }
     
-    void setValue(T val){
+    virtual void setValue(T val){
         buf[curPos] = val;
     }
     
-    void posUp(){
+    virtual void posUp(){
         curPos++;
         if(curPos>=buf.size())curPos=0;
     }
     
-    void posDown(){
+    virtual void posDown(){
         curPos--;
         if(curPos<0)curPos=buf.size()-1;
     }
     
     
-    BufferIterator & begin(){
+    virtual BufferIterator begin(){
         return BufferIterator(buf.begin()+curPos);
     }
     
-    BufferIterator & end(){
+    virtual BufferIterator end(){
         int pos = curPos-1;
         if(pos<0)pos = buf.size()-1;
         return BufferIterator(buf.begin()+pos);
     }
     
-    BufferIterator nthFromBegin(int n){
+    virtual BufferIterator nthFromBegin(int n){
         int nth = curPos+n;
         while(nth>=buf.size()){
             nth -= buf.size();
@@ -68,11 +68,11 @@ public:
         return BufferIterator(buf.begin()+nth);
     }
     
-    T getValue(){return buf[curPos];}
-    T getValue(int idx){return buf[idx];}
+    virtual T getValue(){return buf[curPos];}
+    virtual T getValue(int idx){return buf[idx];}
     
-    int getCurPos(){return curPos;}
-    int getSize(){return buf.size();}
+    virtual int getCurPos(){return curPos;}
+    virtual int getSize(){return buf.size();}
 protected:
     vector<T> buf;
     int curPos;
